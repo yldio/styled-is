48bd796da0ee6bde71bc1fa69202720b1cdc6e61
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2020-10-05 11:03:19 +0200

    update typescript definition

--- index.d.ts
+++ index.d.ts
@@ -6,5 +6,6 @@ export function match<T>(propName: keyof T, propValue: T[keyof T]);
 export function isNot<T>(...propName: BooleanProps<T>[]);
 export function isOr<T>(...propName: BooleanProps<T>[]);
 export function isSomeNot<T>(...propName: BooleanProps<T>[]);
+export function get(propName: string);
 
 export default function is<T>(...propName: BooleanProps<T>[]);
